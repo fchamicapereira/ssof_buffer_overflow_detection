72d64a7f82bd0dedf13fa3dd1a4b479e60cb982e
simaovc <simaovalecardoso@gmail.com>
2018-11-21 22:05:28 +0000

    added 41_ifs to advanced tests

--- public_advanced_tests/41_ifs.c
+++ public_advanced_tests/41_ifs.c
@@ -8,9 +8,12 @@ int main() {
   char buf2[32];
   char buf1[64];
 
-  control = 6;
+  control = 41;
   fgets(buf1, 64, stdin);
-  strncpy(buf2, buf1, 32);
+  if (control ==  41)
+    strcpy(buf2, buf1);
+  else
+    strncpy(buf2, buf1, 32);
 
   return 0;
 }
